4e413641bf1233dff4ef64ce087d049999311573
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-02-16 18:37:49 +0530

    Complete factorial exercise of computer_sc recurs.

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,9 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if(n < 0 || isNaN(n) || !Number.isInteger(n)) return undefined
+    if(n == 0 || n == 1) return 1
+    else{
+        return n * factorial(n-1)
+    }
 };
 
 // Do not edit below this line
