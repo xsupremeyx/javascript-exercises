03578f2be63094582d5dec457bfa5c5e8e4e8a0f
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-02-16 19:38:18 +0530

    Complete totalIntegers exercise of CS/Recursion.

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,31 @@
-const totalIntegers = function() {
+function isPlainObjectOrArr(variable) {
+  return typeof variable === 'object' && variable !== null;
+}
+
+const totalIntegers = function(object) {
+  let count  = 0;
+  if(!isPlainObjectOrArr(object)){
+    return;
+  }
+  for(const key in object){
+    const val = object[key];
+    if(Number.isSafeInteger(val)){
+        count++;
+    }
+    else if(isPlainObjectOrArr(val)){
+        count += totalIntegers(val);
+    }
+  }
+  return count;
+};
+
+
+  
+
+
+const contains = function(object,value) {
   
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
